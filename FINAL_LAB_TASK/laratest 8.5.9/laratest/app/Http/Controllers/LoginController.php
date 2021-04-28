cda40d66afcbc81a6bbd4f0275e72d2b03f50346
RoyShanto <roycse17@gmail.com>
2021-04-28 12:42:03 +0600

    Login Completed (backend)

---
+++ FINAL_LAB_TASK/laratest 8.5.9/laratest/app/Http/Controllers/LoginController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\users;
+
+class LoginController extends Controller
+{
+    public function index(){
+        return view('login.index');
+    }
+
+    public function verify(Request $req){
+        $rules = [
+            'username' => 'required|max:49',
+            'password' => 'required|min:3|max:20|alpha_num'];
+            $this->validate($req, $rules);
+
+        $users = DB::table('users')->where('u_username', $req->username)
+                    ->where('u_password', $req->password)
+                    ->get();
+
+
+
+        if(count($users) > 0){
+
+            $req->session()->put('username', $req->username);
+
+                return redirect('/home');
+                echo "Success";
+        }
+        else{
+            $req->session()->flash('msg', 'invalide user..!');
+            return redirect('/login');
+        }
+
+    }
+}
